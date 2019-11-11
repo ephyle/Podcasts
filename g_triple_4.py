c0de1473de492d05e37bc45fa363e94029d52fbe
ephyle <42876614+ephyle@users.noreply.github.com>
2019-11-11 18:14:51 -0500

    Update g_triple_4.py

--- g_triple_4.py
+++ g_triple_4.py
@@ -11,24 +11,26 @@ graph = Graph()
 with open('triples.csv','r') as file:
     tbuild = csv.reader(file)
     next(tbuild, None)
+    #read every entity in spreadsheet
     for row in tbuild:
+        #if an entity exsists, create basic triples (name, type, uri)
         if row[3] != None:
             Name = row[3]
-        # if Type == "FOAF.Person":
-            # Type = FOAF.Person
             Type = row[4] 
             URI = row[5]
             graph.add( (URIRef(URI), RDF.type, URIRef(Type)) )
             graph.add( (URIRef(URI), FOAF.name, Literal(Name)) ) 
             graph.add( (URIRef(URI), DC.identifier, URIRef(URI)) )
-        if row[6] != None:
-            P2 = row[6]
-            O2 = row[7]
-            graph.add( (URIRef(URI), URIRef(P2), URIRef(O2)) )
-        if row[8] != None:
-            P3 = row[8]
-            O3 = row[9]            
-            graph.add( (URIRef(URI), URIRef(P3), URIRef(O3)) )
+            #if an entity has another statement, add it
+            if row[6] != None:
+                P2 = row[6]
+                O2 = row[7]
+                graph.add( (URIRef(URI), URIRef(P2), URIRef(O2)) )
+            #if an entity has ANOTHER statement, add it
+            if row[8] != None:
+                P3 = row[8]
+                O3 = row[9]            
+                graph.add( (URIRef(URI), URIRef(P3), URIRef(O3)) )
        # print(Type, Name, URI)
 
 for s, p, o in graph:
